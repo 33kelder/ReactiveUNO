7d3a270738c30fb2968a03d7f437b75e6ef27117
Ronald Eekelder <info@oakrefactory.com>
2019-11-27 09:48:03 +0100

    Add project files.

---
+++ ReactiveUNO/ReactiveUNO.Shared/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using ReactiveUI;
+using ReactiveUNO.Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ReactiveUNO
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, IViewFor<MainPageViewModel>
+    {
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty
+            .Register(nameof(ViewModel), typeof(MainPageViewModel), typeof(MainPage), new PropertyMetadata(null));
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ViewModel = new MainPageViewModel();
+
+            this.WhenActivated(disposable =>
+            {
+                this.Bind(ViewModel, x => x.TheText, x => x.TheTextBox.Text)
+                    .DisposeWith(disposable);
+                this.OneWayBind(ViewModel, x => x.TheText, x => x.TheTextBlock.Text)
+                    .DisposeWith(disposable);
+                this.BindCommand(ViewModel, x => x.TheTextCommand, x => x.TheTextButton)
+                    .DisposeWith(disposable);
+            });
+        }
+
+        public MainPageViewModel ViewModel
+        {
+            get => (MainPageViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (MainPageViewModel)value;
+        }
+    }
+}
---
+++ ReactiveUNO/ReactiveUNO.Shared/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReactiveUNO.Shared
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        private string theText = string.Empty;
+        public string TheText
+        {
+            get => theText;
+            set => this.RaiseAndSetIfChanged(ref theText, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> TheTextCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            TheTextCommand = ReactiveCommand
+                .CreateFromObservable(ExecuteTextCommand);
+        }
+
+        private IObservable<Unit> ExecuteTextCommand()
+        {
+            TheText = "Hello ReactiveUI";
+            return Observable.Return(Unit.Default);
+        }
+    }
+}
